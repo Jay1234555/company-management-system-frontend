dee81bfd98e60b9f7b2dc08b87cba389396c2e3e
Jay1234555 <jayeshratwadkar@gmail.com>
2026-09-05 16:16:22 +0530

    Fix CORS for Railway frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,80 +1,18 @@
-
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "../CSS_PAGES/Navbar.css";
-
-function Navbar() {
-
-    const navigate = useNavigate();
-
-    const user = JSON.parse(localStorage.getItem("user"));
-
-    const handleLogout = () => {
-        localStorage.removeItem("user");
-        navigate("/login");
-    };
-
-    return (
-        <nav className="navbar">
-
-            <div className="navbar-container">
-
-                {/* Logo */}
-
-                <Link to="/" className="navbar-logo">
-                    Company
-                </Link>
-
-
-                {/* Navigation */}
-
-                <div className="navbar-links">
-
-                    <Link to="/">Home</Link>
-
-                    <Link to="/about">About</Link>
-
-                    <Link to="/services">Services</Link>
-
-                    <Link to="/projects">Projects</Link>
-
-                    <Link to="/contact">Contact</Link>
-
-
-                    {/* Admin */}
-
-                    {user ? (
-
-                        <>
-                            <Link to="/dashboard">
-                                Dashboard
-                            </Link>
-
-                            <button
-                                className="navbar-logout"
-                                onClick={handleLogout}
-                            >
-                                Logout
-                            </button>
-                        </>
-
-                    ) : (
-
-                        <Link
-                            to="/login"
-                            className="navbar-login"
-                        >
-                            Login
-                        </Link>
-
-                    )}
-
+import React from "react"; 
+import { Link } from "react-router-dom";
+ import "../CSS_PAGES/Navbar.css";
+  function Navbar()
+   { const user = JSON.parse(localStorage.getItem("user"));
+     return ( <nav className="navbar"> <div className="navbar-container">
+         {/* Logo */} <Link to="/" className="navbar-logo"> Company </Link> 
+         {/* Navigation */} <div className="navbar-links"> 
+            <Link to="/">Home</Link> 
+            <Link to="/about">About</Link>
+             <Link to="/services">Services</Link>
+              <Link to="/projects">Projects</Link>
+               <Link to="/contact">Contact</Link>
+                {/* Login */} {!user && ( <Link to="/login" className="navbar-login" > Login </Link> )} 
+                </div> 
                 </div>
-
-            </div>
-
-        </nav>
-    );
-}
-
-export default Navbar;
+                 </nav> ); }
+ export default Navbar;
